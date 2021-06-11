04941f0bbd8e530001202324da2c2569acd8dec6
Thomas Lyseggen <thomas.lyseggen@tripletex.no>
2021-06-11 10:59:48 +0200

    Add model classes and a service

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
+import model.Creature;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        //Define a simple MultiLayerNetwork:
+        CreateService service = new CreateService();
+        Creature creature1 = service.createCreature(1);
+        Creature creature2 = service.createCreature(2);
+        service.saveCreature(creature1);
+        service.saveCreature(creature2);
+        Creature c1 = service.restoreCreature(1);
+        Creature c2 = service.restoreCreature(2);
+        System.out.println("Saved and loaded parameters are equal:      " + creature1.getBrain().getNet().params().equals(c1.getBrain().getNet().params()));
+        System.out.println("Saved and loaded configurations are equal:  " + creature1.getBrain().getNet().getLayerWiseConfigurations().equals(c1.getBrain().getNet().getLayerWiseConfigurations()));
+        System.out.println("Saved and loaded parameters are equal:      " + creature2.getBrain().getNet().params().equals(c2.getBrain().getNet().params()));
+        System.out.println("Saved and loaded configurations are equal:  " + creature2.getBrain().getNet().getLayerWiseConfigurations().equals(c2.getBrain().getNet().getLayerWiseConfigurations()));
     }
+
 }
